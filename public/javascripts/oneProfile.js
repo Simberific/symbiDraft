0f3db4248ad42db36d2cd9cbb08d80e74eca6161
Simone <simone.berkower@gmail.com>
2014-12-17 15:31:33 -0500

    Added slider to OneProfile page

--- public/javascripts/oneProfile.js
+++ public/javascripts/oneProfile.js
@@ -33,3 +33,40 @@ $( document ).ready(function() {
 $('.carousel').carousel({
     interval: false
 });
+
+/* Rate slider */
+$(document).ready(function() {
+    $("#slider").slider({
+        animate: 1,
+        value:3,
+        min: 1,
+        max: 5,
+        step: 1,
+        slide: function(event, ui) {
+            update(1,ui.value); //changed
+        }
+
+    });
+
+
+    //Added, set initial value.
+    $("#amount").val(3);
+    $("#amount-label").text(0);
+    update();
+});
+
+
+//changed. now with parameter
+function update(slider,val) {
+    //changed. Now, directly take value from ui.value. if not set (initial, will use current value.)
+    var $amount = slider == 1?val:$("#amount").val();
+
+    /* commented
+     $amount = $( "#slider" ).slider( "value" );
+     $duration = $( "#slider2" ).slider( "value" );
+     */
+
+
+    $('#slider a').html('<label><span class="glyphicon glyphicon-chevron-left"></span> '+$amount+' <span class="glyphicon glyphicon-chevron-right"></span></label>');
+}
+$('.ui-slider-handle').draggable();
