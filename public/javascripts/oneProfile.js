cbf91f3c3914711ff5659c5467702ebf12af668c
Simone <simone.berkower@gmail.com>
2014-12-21 12:53:56 -0500

    Changed small slider to star ratings, and did optimizations for the mobile view

--- public/javascripts/oneProfile.js
+++ public/javascripts/oneProfile.js
@@ -17,6 +17,21 @@ $(document).ready(function() {
     });
 });
 
+// Need to fix this - just added:
+$(document).ready(function() {
+    var $btnSets = $('#responsiveSm'),
+        $btnLinks = $btnSets.find('a');
+
+    $btnLinks.click(function(e) {
+        e.preventDefault();
+        $(this).siblings('a.active').removeClass("active");
+        $(this).addClass("active");
+        var index = $(this).index();
+        $("div.user-menu>div.user-menu-content").removeClass("active");
+        $("div.user-menu>div.user-menu-content").eq(index).addClass("active");
+    });
+});
+
 $( document ).ready(function() {
     $("[rel='tooltip']").tooltip();
 
